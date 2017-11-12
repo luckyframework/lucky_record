8f92ded28558eebef57b9206ad342515d42ee2cf
Flinn <flinn@actsasflinn.com>
2017-11-12 08:21:13 -0500

    add custom messages for validations

--- src/lucky_record/validations.cr
+++ src/lucky_record/validations.cr
@@ -1,27 +1,27 @@
 module LuckyRecord::Validations
-  private def validate_required(*fields)
+  private def validate_required(*fields, message = "is required")
     fields.each do |field|
       if field.value.blank?
-        field.add_error "is required"
+        field.add_error message
       end
     end
   end
 
-  private def validate_acceptance_of(field : LuckyRecord::Field(Bool))
+  private def validate_acceptance_of(field : LuckyRecord::Field(Bool), message = "must be accepted")
     if field.value == false
-      field.add_error "must be accepted"
+      field.add_error message
     end
   end
 
-  macro validate_confirmation_of(field_name)
+  macro validate_confirmation_of(field_name, message = "must match")
     if {{ field_name.id }}.value != {{ field_name.id }}_confirmation.value
-      {{ field_name }}.add_error "must match"
+      {{ field_name }}.add_error {{ message }}
     end
   end
 
-  private def validate_inclusions_of(field, in allowed_values)
+  private def validate_inclusions_of(field, in allowed_values, message = "is invalid")
     if !allowed_values.includes? field.value
-      field.add_error "is invalid"
+      field.add_error message
     end
   end
 end
