4df860ee0a4961d399d6ac3d8cd15e7ffeffce22
mikeeus <itsmikias@gmail.com>
2017-11-29 17:55:17 +0300

    Pass nilable to field call

--- src/lucky_record/associations.cr
+++ src/lucky_record/associations.cr
@@ -8,14 +8,21 @@ module LuckyRecord::Associations
   end
 
   macro belongs_to(type_declaration)
+    {% assoc_name = type_declaration.var }
+
     {% if type_declaration.type.is_a?(Union) %}
       {% model = type_declaration.type.types.first %}
+      {% nilable = true %}
     {% else %}
       {% model = type_declaration.type %}
+      {% nilable = false %}
     {% end %}
 
-    {% assoc_name = type_declaration.var }
-    field {{ assoc_name.id }}_id : Int32
+    {% if nilable %}
+      field {{ assoc_name.id }}_id : Int32?
+    {% else %}
+      field {{ assoc_name.id }}_id : Int32
+    {% end %}
 
     def {{ assoc_name.id }}
       {{ model }}::BaseQuery.new.find({{ assoc_name.id }}_id)
