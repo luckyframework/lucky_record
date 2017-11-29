19c7aa57af97283b2b804bae49d133357dc22bdf
mikeeus <itsmikias@gmail.com>
2017-11-29 17:51:07 +0300

    Check for Union type declaration

--- src/lucky_record/associations.cr
+++ src/lucky_record/associations.cr
@@ -8,9 +8,13 @@ module LuckyRecord::Associations
   end
 
   macro belongs_to(type_declaration)
-    {% assoc_name = type_declaration.var }
-    {% model = type_declaration.type %}
+    {% if type_declaration.type.is_a?(Union) %}
+      {% model = type_declaration.type.types.first %}
+    {% else %}
+      {% model = type_declaration.type %}
+    {% end %}
 
+    {% assoc_name = type_declaration.var }
     field {{ assoc_name.id }}_id : Int32
 
     def {{ assoc_name.id }}
