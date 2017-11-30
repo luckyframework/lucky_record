82d232ded510ff438a633de62b54e1ae949c2e11
mikeeus <itsmikias@gmail.com>
2017-11-30 21:40:48 +0300

    Refactor form into BaseBox and update specs

--- spec/support/employee_box.cr
+++ spec/support/employee_box.cr
@@ -1,26 +1,5 @@
-class EmployeeBox
-  getter form
-
-  @form = Employee::BaseForm.new
-
+class EmployeeBox < BaseBox
   def initialize
     name "Humble Employee"
-  end
-
-  def self.save
-    new.save
-  end
-
-  def save
-    if form.save
-      form.record.not_nil!
-    else
-      raise "Did not save. Make sure fields are valid"
-    end
-  end
-
-  macro method_missing(call)
-    form.{{ call.name.id }}.value = {{ call.args.first }}
-    self
   end
 end
--- spec/support/manager_box.cr
+++ spec/support/manager_box.cr
@@ -1,26 +1,5 @@
-class ManagerBox
-  getter form
-
-  @form = Manager::BaseForm.new
-
+class ManagerBox < BaseBox
   def initialize
     name "Mr. Manager"
-  end
-
-  def self.save
-    new.save
-  end
-
-  def save
-    if form.save
-      form.record.not_nil!
-    else
-      raise "Did not save. Make sure fields are valid"
-    end
-  end
-
-  macro method_missing(call)
-    form.{{ call.name.id }}.value = {{ call.args.first }}
-    self
   end
 end
