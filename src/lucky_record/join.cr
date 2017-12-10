ed873f144ea1298f1053d020cbabdd49042a10cd
mikeeus <itsmikias@gmail.com>
2017-12-10 15:02:07 +0300

    Ensure table names are pluralized to handle belongs_to association

--- src/lucky_record/join.cr
+++ src/lucky_record/join.cr
@@ -4,34 +4,45 @@ module LuckyRecord::Join
   abstract class SqlClause
     getter :from_table, :to_table, :from_column, :to_column
 
-    def initialize(@from_table : Symbol, @to_table : Symbol, @from_column : Symbol? = nil, @to_column : Symbol? = nil, @comparison : String? = "=", @using : Array(Symbol) = [] of Symbol)
+    def initialize(@from : Symbol, @to : Symbol, @from_column : Symbol? = nil, @to_column : Symbol? = nil, @comparison : String? = "=", @using : Array(Symbol) = [] of Symbol)
     end
 
     abstract def join_type : String
 
     def to_sql
       if @using.any?
-        "#{join_type} JOIN #{@to_table} USING (#{@using.join(',')})"
+        "#{join_type} JOIN #{to_table} USING (#{@using.join(',')})"
       else
-        "#{join_type} JOIN #{@to_table} ON #{from_column} #{@comparison} #{to_column}"
+        "#{join_type} JOIN #{to_table} ON #{from_column} #{@comparison} #{to_column}"
       end
+    end
+
+    def to_table
+      LuckyInflector::Inflector.pluralize(@to)
+    end
+
+    def from_table
+      LuckyInflector::Inflector.pluralize(@from)
     end
 
     def from_column
       if @from_column
-        "#{@from_table}.#{@from_column}"
+        "#{from_table}.#{@from_column}"
       else
-        "#{@from_table}.id"
+        "#{from_table}.id"
       end
     end
 
     def to_column
       if @to_column
-        "#{@to_table}.#{@to_column}"
+        "#{to_table}.#{@to_column}"
       else
-        foreign_key = LuckyInflector::Inflector.singularize(@from_table) + "_id"
-        "#{@to_table}.#{foreign_key}"
+        "#{to_table}.#{default_foreign_key}"
       end
+    end
+
+    def default_foreign_key
+      LuckyInflector::Inflector.singularize(@from) + "_id"
     end
   end
 
