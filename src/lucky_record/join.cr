86bbe0bac8e74ad30d7082bb3023fc7301939a19
mikeeus <itsmikias@gmail.com>
2017-12-10 16:37:19 +0300

    Remove unnecessary pluralization

--- src/lucky_record/join.cr
+++ src/lucky_record/join.cr
@@ -2,7 +2,7 @@ require "lucky_inflector"
 
 module LuckyRecord::Join
   abstract class SqlClause
-    getter :from_table, :to_table, :from_column, :to_column
+    getter :from, :to, :from_column, :to_column
 
     def initialize(@from : Symbol, @to : Symbol, @primary_key : Symbol? = nil, @foreign_key : Symbol? = nil, @comparison : String? = "=", @using : Array(Symbol) = [] of Symbol)
     end
@@ -11,26 +11,18 @@ module LuckyRecord::Join
 
     def to_sql
       if @using.any?
-        %(#{join_type} JOIN #{to_table} USING (#{@using.join(", ")}))
+        %(#{join_type} JOIN #{@to} USING (#{@using.join(", ")}))
       else
-        "#{join_type} JOIN #{to_table} ON #{from_column} #{@comparison} #{to_column}"
+        "#{join_type} JOIN #{@to} ON #{from_column} #{@comparison} #{to_column}"
       end
     end
 
-    def from_table
-      LuckyInflector::Inflector.pluralize(@from)
-    end
-
-    def to_table
-      LuckyInflector::Inflector.pluralize(@to)
-    end
-
     def from_column
-      "#{from_table}.#{@primary_key || "id"}"
+      "#{@from}.#{@primary_key || "id"}"
     end
 
     def to_column
-      "#{to_table}.#{@foreign_key || default_foreign_key}"
+      "#{@to}.#{@foreign_key || default_foreign_key}"
     end
 
     def default_foreign_key
