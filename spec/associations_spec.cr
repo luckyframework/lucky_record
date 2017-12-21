eda9609515bcc8ebb6edd552428e7b07b8a59113
mikeeus <itsmikias@gmail.com>
2017-12-21 14:51:05 +0300

    Add has_one association with incomplete specs
    simplify SignInCredentials
    
    Create admin table, model and box
    
    Add foreign_key option to has_one macro
    
    Remove unnecessary method
    
    Run crystal tool format

--- spec/associations_spec.cr
+++ spec/associations_spec.cr
@@ -1,5 +1,11 @@
 require "./spec_helper"
 
+# class CredentialedUser < User
+#   table users do
+#     has_one sign_in_credential : SignInCredential
+#   end
+# end
+
 describe LuckyRecord::Model do
   it "gets the related records" do
     post = PostBox.save
@@ -24,5 +30,29 @@ describe LuckyRecord::Model do
   it "returns nil for nilable association that doesn't exist" do
     employee = EmployeeBox.new.save
     employee.manager.should eq nil
+  end
+
+  describe "has_one" do
+    context "missing association" do
+      it "raises if association is not nilable" do
+        credentialed = AdminBox.save
+        expect_raises Exception, "Could not find first record in sign_in_credentials" do
+          credentialed.sign_in_credential
+        end
+      end
+
+      it "returns nil if association is nilable" do
+        possibly_credentialed = UserBox.save
+        possibly_credentialed.sign_in_credential.should be_nil
+      end
+    end
+
+    context "existing association" do
+      it "returns associated model" do
+        user = UserBox.save
+        credentials = SignInCredentialBox.new.user_id(user.id).save
+        user.sign_in_credential.should eq credentials
+      end
+    end
   end
 end
