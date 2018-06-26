16a0024c24dbecd255c78f9f3f7d0e8b68d24156
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-06-26 22:11:40 +0200

    Add comment in migration to remind for lucky_migrator change

--- db/migrations/20180625202051_create_line_item.cr
+++ db/migrations/20180625202051_create_line_item.cr
@@ -1,5 +1,9 @@
 class CreateLineItem::V20180625202051 < LuckyMigrator::Migration::V1
   def migrate
+    # Needs support in lucky_migrator
+    # create :line_items, primary_key_type: :uuid do
+    #   add name : String
+    # end
     execute <<-SQL
       CREATE TABLE line_items (
         id uuid PRIMARY KEY,
